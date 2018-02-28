888f4b89b53c06fdc245082cc6f876d31d7de0c9
CodingDab <nickallbritton8664@gmail.com>
2018-02-28 11:40:25 -0600

    created title image and a simple dev message at bottom of menu state

--- SnakeJava/src/Snake/Menu.java
+++ SnakeJava/src/Snake/Menu.java
@@ -6,6 +6,7 @@ import org.newdawn.slick.state.*;
 public class Menu extends BasicGameState
 {
 	private int id;
+	private Image titleImage;
 	public Menu(int id)
 	{
 		this.id = id;
@@ -14,13 +15,15 @@ public class Menu extends BasicGameState
 	public void init(GameContainer gc, StateBasedGame sbg)
 		throws SlickException
 	{
-		
+		titleImage = new Image("../SnakeJava/res/images/snaketitleimage.png");
 	}
 	
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
 		throws SlickException
 	{
-		
+		g.setColor(Color.blue);
+		g.drawString("Developed by 'Java' 2018.", 265, 580);
+		g.drawImage(titleImage, gc.getScreenWidth()/2 - titleImage.getWidth() * 7/6, 50f);
 	}
 	
 	public void update(GameContainer gc, StateBasedGame sbg, int delta)
