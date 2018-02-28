9f2abff512f5147e91171df4b6b25dedcaf3473b
CodingDab <nickallbritton8664@gmail.com>
2018-02-28 13:18:31 -0600

    building the structure of the snake

---
+++ SnakeJava/src/Snake/Link.java
@@ -0,0 +1,18 @@
+package Snake;
+
+import org.newdawn.slick.*;
+
+public class Link
+{
+	private Tile loc;
+	
+	public Link(Tile in_loc)
+	{
+		loc = in_loc;
+	}
+	
+	public void draw(Graphics g)
+	{
+		
+	}
+}
--- SnakeJava/src/Snake/Snake.java
+++ SnakeJava/src/Snake/Snake.java
@@ -4,12 +4,27 @@ import org.newdawn.slick.*;
 
 public class Snake
 {
+	private Link[] snakeLinks;
 	public Snake(int length)
 	{
-		
+		snakeLinks = new Link[length];
 	}
 	public void draw(Graphics g)
 	{
+		for(int i = 0; i < snakeLinks.length; i++)
+		{
+			snakeLinks[i].draw(g);
+		}
+	}
+	public void Add(Link newLink)
+	{
+		Link[] temp = new Link[snakeLinks.length + 1];
+		for(int i = 0; i < snakeLinks.length; i++)
+		{
+			temp[i] = snakeLinks[i];
+		}
+		temp[temp.length] = newLink;
 		
+		snakeLinks = temp;
 	}
 }
